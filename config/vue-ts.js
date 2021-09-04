904b299806b10816e46bd3a21a6f3a349f4fedf0
flow <senb.zhuang@gmail.com>
2021-09-05 02:14:57 +0800

    代码优化

--- config/vue-ts.js
+++ config/vue-ts.js
@@ -1,25 +1,26 @@
 module.exports = {
-  extends: ['plugin:vue/recommended', require.resolve('./common-ts')],
-  rules: {
-    'vue/max-attributes-per-line': 'off', // 与prettier冲突
-    'vue/singleline-html-element-content-newline': 'off',
-    'vue/no-v-html': 'off',
-    'vue/require-default-prop': 'off',
-    'vue/html-closing-bracket-spacing': 'error',
-    'vue/html-self-closing': ['error', {
-      html: {
-        void: 'any',
-        normal: 'any',
-        component: 'always',
-      },
-    }],
-  },
+  extends: require.resolve('./common-ts'),
   overrides: [
     {
       files: ['*.vue'],
       parser: 'vue-eslint-parser',
       parserOptions: {
         parser: '@typescript-eslint/parser', // 解析<script>
+      },
+      extends: 'plugin:vue/recommended',
+      rules: {
+        'vue/max-attributes-per-line': 'off', // 与prettier冲突
+        'vue/singleline-html-element-content-newline': 'off',
+        'vue/no-v-html': 'off',
+        'vue/require-default-prop': 'off',
+        'vue/html-closing-bracket-spacing': 'error',
+        'vue/html-self-closing': ['error', {
+          html: {
+            void: 'any',
+            normal: 'any',
+            component: 'always',
+          },
+        }],
       },
     },
   ],
--- config/vue.js
+++ config/vue.js
@@ -1,23 +1,24 @@
 module.exports = {
-  extends: ['plugin:vue/recommended', require.resolve('./common')],
-  rules: {
-    'vue/max-attributes-per-line': 'off', // 与prettier冲突
-    'vue/singleline-html-element-content-newline': 'off',
-    'vue/no-v-html': 'off',
-    'vue/require-default-prop': 'off',
-    'vue/html-closing-bracket-spacing': 'error',
-    'vue/html-self-closing': ['error', {
-      html: {
-        void: 'any',
-        normal: 'any',
-        component: 'always',
-      },
-    }],
-  },
+  extends: require.resolve('./common'),
   overrides: [
     {
       files: ['*.vue'],
       parser: 'vue-eslint-parser',
+      extends: 'plugin:vue/recommended',
+      rules: {
+        'vue/max-attributes-per-line': 'off', // 与prettier冲突
+        'vue/singleline-html-element-content-newline': 'off',
+        'vue/no-v-html': 'off',
+        'vue/require-default-prop': 'off',
+        'vue/html-closing-bracket-spacing': 'error',
+        'vue/html-self-closing': ['error', {
+          html: {
+            void: 'any',
+            normal: 'any',
+            component: 'always',
+          },
+        }],
+      },
     },
   ],
 };
